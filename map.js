535128383a350aae7ad60a7af051281c915f39bb
Md. Sumon Mia <md.sumonmia.cse.iu@gmail.com>
2022-03-08 18:01:47 +0600

    first commit

---
+++ map.js
@@ -0,0 +1,13 @@
+const numbers=[];
+const newNumbers=numbers.forEach(x=>x)
+console.log(newNumbers);
+
+let person =null;
+console.log(person ? person : "person is null");
+
+const obj = {a:1};
+console.log(Object.keys(obj).length===0);
+
+const age= '33';
+const y=+age;
+console.log(y);
